1dc0cee266237083a5b4496f0a03f39a1a670b6c
zq-jhon <350037310@qq.com>
2019-11-11 11:40:45 +0800

    chore: modify script lunch_blog.js

--- scripts/lunch_blog.js
+++ scripts/lunch_blog.js
@@ -1,5 +1,5 @@
 // This file just for Tecent Cloud CVM use only.
 const { exec } = require('child_process');
-exec(`sudo http-server ./ZQ-jhon.github.io/ -p 80`, function() {
+exec(`sudo http-server ./ -p 80`, function() {
     console.log(`http static server was listening at localhost:80, https://ZQ-jhon.club as reflect url`);
 });
